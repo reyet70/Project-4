7523d9da937003f2bf8ae07eb3ad71cedd2d1ecd
ferron99 <nferro@terpmail.umd.edu>
2015-11-14 13:35:03 -0500

    Update explosion_frames.java

--- explosion_frames.java
+++ explosion_frames.java
@@ -1,3 +1,6 @@
+//explosion animation development
+//Nick Ferro 11-14-15
+
 float x,y;
 
 void setup(){
@@ -7,7 +10,7 @@ void setup(){
 }
 
 void draw(){
-  //frame 5
+  /*//frame 5
   fill(255,255,0);
   beginShape();
   vertex(x-24,y);
@@ -27,4 +30,30 @@ void draw(){
 
   //ellipse(x,y,48,48);
   //ellipse(x,y+24,24,24);
+  */
+  //frame 6
+  fill(255);
+  beginShape();
+  vertex(x-24,y);
+  bezierVertex(x-24,y-13,x-13,y-24,x,y-24);   //outer top left quadrent
+  vertex(x,y-24);
+  bezierVertex(x+13,y-24,x+24,y-13,x+24,y);   //outer top right quadrant
+  vertex(x+24,y);
+  bezierVertex(x+24,y+13,x+13,y+16,x+18,y+16);  ////outer bottom right quadrant
+  vertex(x+18,y+16);
+  bezierVertex(x+18,y+6,x+10,y-2,x,y-2);      //inner right
+  vertex(x,y-2);
+  bezierVertex(x-10,y-2,x-18,y+6,x-18,y+16);  //inner left
+  vertex(x-18,y+16);
+  bezierVertex(x-13,y+21,x-24,y+13,x-24,y);  //outer bottom left
+  
+  endShape(CLOSE);
+  
+  //ellipse(x,y,48,48);
+  //ellipse(x,y+16,36,36);
+  
+  
+  
+  
+  
 }
